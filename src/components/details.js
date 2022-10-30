43c7d061c5f424d64be3ea1ea4a7c1ef9916e42a
Shyamalm88 <shyamalm88@gmail.com>
2022-10-30 05:43:18 +0530

    back

--- src/components/details.js
+++ src/components/details.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import {
   fetchDetailsData,
   repoDetailsSuccessAction,
@@ -24,45 +25,67 @@ function Details({ detailsData }) {
 
   return (
     <div className="mt-2 container">
+      <Link to="/">
+        <i className="fa fa-chevron-left" aria-hidden="true"></i> Back
+      </Link>
       <div className="card">
         <div className="card-body">
-          <h5 className="card-title">
-            <a
-              href={`${detailsData.data.html_url}`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fa fa-book m-r-5" aria-hidden="true"></i>
-              {detailsData.data.full_name}
-            </a>
-          </h5>
-          <p className="card-text">{detailsData.data.description}</p>
+          {detailsData.data.full_name && (
+            <h5 className="card-title">
+              <a
+                href={`${detailsData.data.html_url}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fa fa-book m-r-5" aria-hidden="true"></i>
+                {detailsData.data.full_name}
+              </a>
+            </h5>
+          )}
+          {detailsData.data.description && (
+            <p className="card-text">{detailsData.data.description}</p>
+          )}
+
           <div className="attributes mb-2">
-            <span
-              className="card-subtitle mb-2 m-r-15 text-danger"
-              title="Language"
-            >
-              <i className="fa fa-code m-r-5" aria-hidden="true"></i>
-              {detailsData.data.language}
-            </span>
-            <span className="card-subtitle mb-2 m-r-15 text-muted" title="Fork">
-              <i className="fa fa-code-fork m-r-5" aria-hidden="true"></i>
-              {detailsData.data.forks}
-            </span>
-            <span
-              className="card-subtitle mb-2 m-r-15 text-muted"
-              title="Watcher"
-            >
-              <i className="fa fa-eye m-r-5" aria-hidden="true"></i>
-              {detailsData.data.watchers}
-            </span>
-            <span
-              className="card-subtitle mb-2 m-r-15 text-muted"
-              title="Open Issues"
-            >
-              <i className="fa fa-bug m-r-5" aria-hidden="true"></i>
-              {detailsData.data.open_issues}
-            </span>
+            {detailsData.data.language && (
+              <span
+                className="card-subtitle mb-2 m-r-15 text-danger"
+                title="Language"
+              >
+                <i className="fa fa-code m-r-5" aria-hidden="true"></i>
+                {detailsData.data.language}
+              </span>
+            )}
+
+            {detailsData.data.forks && (
+              <span
+                className="card-subtitle mb-2 m-r-15 text-muted"
+                title="Fork"
+              >
+                <i className="fa fa-code-fork m-r-5" aria-hidden="true"></i>
+                {detailsData.data.forks}
+              </span>
+            )}
+
+            {detailsData.data.watchers && (
+              <span
+                className="card-subtitle mb-2 m-r-15 text-muted"
+                title="Watcher"
+              >
+                <i className="fa fa-eye m-r-5" aria-hidden="true"></i>
+                {detailsData.data.watchers}
+              </span>
+            )}
+
+            {detailsData.data.open_issues && (
+              <span
+                className="card-subtitle mb-2 m-r-15 text-muted"
+                title="Open Issues"
+              >
+                <i className="fa fa-bug m-r-5" aria-hidden="true"></i>
+                {detailsData.data.open_issues}
+              </span>
+            )}
           </div>
         </div>
       </div>
